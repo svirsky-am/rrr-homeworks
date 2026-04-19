721f3f6ddd5daf27134607b9e55b5c4b1f20dcaa
svirsky <aouta91alk@gmail.com>
2026-04-20 01:40:26 +0300

    tuneup postgre

--- blog-project/run_build_test_mod3.mk
+++ blog-project/run_build_test_mod3.mk
@@ -9,3 +9,23 @@ mod3_build_all:
 PHONY: mod3_server_test
 mod3_server_test: 
 	cargo test -p  "blog-server"
+
+export HOST=127.0.0.1
+export PORT=8080
+export JWT_SECRET=dev_super_secret_change_me_please
+export  CORS_ORIGINS=http://localhost:3000
+export DATABASE_URL=postgres://blog_admin:blog_pass@127.0.0.1:5432/bank_api
+# export SECRET_ADMIN_PG_PASS=blog_pass
+# JWT_SECRET=dev_super_secret_change_me_please
+# CORS_ORIGINS=http://localhost:3000
+EXCHANGE_API_URL=https://api.exchangerate-api.com/v4/latest
+
+
+PHONY: mod3_server_run
+mod3_server_run: 
+	psql -d postgres -c "DROP DATABASE bank_api;"
+	psql -d postgres -c "CREATE DATABASE bank_api;"
+	cargo run -p  "blog-server"
+
+# 	psql -U $$USER -h localhost -d postgres "GRANT ALL PRIVILEGES ON DATABASE bank_api TO blog_admin;"
+
