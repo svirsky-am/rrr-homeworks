818ad9de5cac613779b819790181bbc95b97f92e
svirsky <aouta91alk@gmail.com>
2026-05-18 07:19:12 +0300

    broken-app-5.1-fixup-criterion-for-broken-app

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,5 +1,10 @@
-use broken_app::{algo, sum_even};
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use broken_app::{algo, sum_even, 
+    normalize, 
+    // normalize_by_reference_app,  
+    // normalize_faster_new_alt, 
+    // average_positive_by_reference_app, 
+    average_positive};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion, black_box};
 
 fn bench_sum_even(c: &mut Criterion) {
     let data: Vec<i64> = (0..50_000).collect();
@@ -23,5 +28,56 @@ fn bench_dedup(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_sum_even, bench_fib, bench_dedup);
+static STRING_TO_NORMALIZE: &str = " Hel\n\nlo \t\tWor\t\tld  Hel\n\nlo \t\tWor\t\tld  Hel\n\nlo \t\tWor\t\tld  Hel\n\nlo \t\tWor\t\tld  Hel\n\nlo \t\tWor\t\tld  Hel\n\nlo \t\tWor\t\tld  Hel\n\nlo \t\tWor\t\tld ";
+
+
+
+fn bench_normalize_with_hot_fix(c: &mut Criterion) {
+    // let data: Vec<u64> = (0..5_000).flat_map(|n| [n, n]).collect();
+    c.bench_function("bench_normalize_with_hot_fix", |b| {
+
+        b.iter(|| normalize(black_box(&STRING_TO_NORMALIZE)));
+
+    });
+}
+
+
+// fn bench_normalize_by_reference_app(c: &mut Criterion) {
+//     // let data: Vec<u64> = (0..5_000).flat_map(|n| [n, n]).collect();
+//     c.bench_function("normalize_by_reference_app", |b| {
+
+//         b.iter(|| normalize_by_reference_app(black_box(&STRING_TO_NORMALIZE)));
+
+//     });
+// }
+
+// fn bench_normalize_faster_new_alt(c: &mut Criterion) {
+//     // let data: Vec<u64> = (0..5_000).flat_map(|n| [n, n]).collect();
+//     c.bench_function("normalize_faster_new_alt", |b| {
+//         b.iter(|| normalize_faster_new_alt(black_box(&STRING_TO_NORMALIZE)));
+//     });
+// }
+
+fn bench_average_positive(c: &mut Criterion) {
+    let nums = [-5, 5, 15, -5, 5, 15,-5, 5, 15,-5, 5, 15,-5, 5, 15-5, 5, 15];
+
+    c.bench_function("bench_average_positive", |b| {
+        b.iter(|| average_positive(black_box(&nums)));
+    });
+}
+
+
+// fn bench_average_positive_by_reference_app(c: &mut Criterion) {
+//     let nums = [-5, 5, 15, -5, 5, 15,-5, 5, 15,-5, 5, 15,-5, 5, 15-5, 5, 15];
+//     c.bench_function("bench_average_positive_by_reference_app", |b| {
+//         b.iter(|| average_positive_by_reference_app(black_box(&nums)));
+//     });
+// }
+
+criterion_group!(benches, bench_sum_even, bench_fib, bench_dedup, 
+    // bench_normalize_by_reference_app,
+    // bench_normalize_faster_new_alt,
+    bench_average_positive,
+    // bench_average_positive_by_reference_app
+);
 criterion_main!(benches);
