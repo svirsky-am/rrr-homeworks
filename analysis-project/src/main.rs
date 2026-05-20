71e8837cc17bad45e6bc54a851adf77fd526a0f8
svirsky <aouta91alk@gmail.com>
2026-05-21 00:13:31 +0300

    fix 1
    Использование лишних вызовов clone() вместо ссылок.

--- analysis-project/src/main.rs
+++ analysis-project/src/main.rs
@@ -51,7 +51,7 @@
 fn main() {
     println!("Placeholder для экспериментов с cli");
 
-    let parsing_demo = r#"[UserBackets{"user_id":"Bob","backets":[Backet{"asset_id":"milk","count":3,},],},]"#.to_string();
+    let parsing_demo = r#"[UserBackets{"user_id":"Bob","backets":[Backet{"asset_id":"milk","count":3,},],},]"#;
     let announcements = analysis::parse::just_parse_anouncements(parsing_demo).unwrap();
     println!("demo-parsed: {:?}", announcements);
 
@@ -60,7 +60,7 @@ fn main() {
     println!("Trying opening file '{}' from directory '{}'", filename, std::env::current_dir().unwrap().to_string_lossy());
     let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> = std::rc::Rc::new(std::cell::RefCell::new(Box::new(std::fs::File::open(filename).unwrap())));
 
-    let logs = analysis::read_log(file.clone(), analysis::READ_MODE_ALL, vec![]);
+    let logs = analysis::read_log(file, analysis::READ_MODE_ALL, vec![]);
     println!("got logs:");
     logs.iter().for_each(|parsed| println!("  {:?}", parsed));
 }
